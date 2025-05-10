0558258963fa14070489f13153e3c066ae7697e8
Vishvijay <vishvijayps123@gmail.com>
2025-05-10 16:02:24 +0530

    Uploading the pyhtonanywhere web

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,12 +1,12 @@
       // Create a hidden iframe to preload the main page
       const iframe = document.createElement("iframe");
-      iframe.src = "home.html";
+      iframe.src = "templates/home.html";
       iframe.style.display = "none";
 
       iframe.onload = function () {
         // Add a delay (in milliseconds) after the page loads
         setTimeout(function () {
-          window.location.href = "home.html";
+          window.location.href = "templates/home.html";
         }, 2000);
       };
 
